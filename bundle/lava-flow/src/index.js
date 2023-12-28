3ddf8803a4c94eb1511dda92aa8c62ec4b20a3ff
SomeOats <wwpowers01@gmail.com>
2023-12-28 16:07:21 -0500

    removing excess files

--- bundle/lava-flow/src/index.js
+++
@@ -1,9 +0,0 @@
-import LavaFlow from './lava-flow.js';
-Hooks.on('renderJournalDirectory', function (app, html) {
-	try {
-		LavaFlow.createUIElements(html);
-	}
-	catch (e) {
-		LavaFlow.errorHandling(e);
-	}
-});
